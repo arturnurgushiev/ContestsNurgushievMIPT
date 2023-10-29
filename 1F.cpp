7f36b3f6653be802ea7f2a777993f81e3d2afd3b
arturnurgushiev <142039719+arturnurgushiev@users.noreply.github.com>
2023-10-29 11:03:28 +0300

    Update 1F.cpp

--- 1F.cpp
+++ 1F.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <vector>
 
-const int kBase = 10;
-const int kTen = 10;
-
 int SmallMassiveSizeFiveMedian(std::vector<int>& number) {
   if (number[0] > number[1]) {
     std::swap(number[0], number[1]);
@@ -27,8 +24,8 @@ int SmallMassiveSizeFiveMedian(std::vector<int>& number) {
   return std::min(number[1], number[3]);
 }
 
-int SmallQuickSelect(std::vector<int>& number, int left, int right,
-                     int which_one) {
+int SmallQuickSelectStupidSort(std::vector<int>& number, int left, int right,
+                               int which_one) {
   for (int i = left; i < right; ++i) {
     int imin = i;
     for (int j = i + 1; j < right; ++j) {
@@ -47,8 +44,6 @@ int SmallQuickSelect(std::vector<int>& number, int left, int right,
 std::pair<int, int> Partition(std::vector<int>& number, int left, int right,
                               int pivot) {
   int pointer = -1;
-  int first;
-  int second;
   for (int i = left; i < right; ++i) {
     if (number[i] >= pivot) {
       continue;
@@ -56,21 +51,22 @@ std::pair<int, int> Partition(std::vector<int>& number, int left, int right,
     ++pointer;
     std::swap(number[i], number[left + pointer]);
   }
-  first = pointer;
+  int first = pointer;
   for (int i = left + pointer + 1; i < right; ++i) {
-    if (number[i] > pivot) {
-      continue;
+    if (number[i] <= pivot) {
+      ++pointer;
+      std::swap(number[i], number[left + pointer]);
     }
-    ++pointer;
-    std::swap(number[i], number[left + pointer]);
   }
-  second = pointer;
+  int second = pointer;
   return {first, second};
 }
 
-int DQS(std::vector<int>& number, int left, int right, int which_one) {
+int DQuickSelect(std::vector<int>& number, int left, int right, int which_one) {
+  const int kSmallerMassiveMedianHelpConst = 10;
+  const int kBase = 10;
   if (right - left <= kBase) {
-    return SmallQuickSelect(number, left, right, which_one);
+    return SmallQuickSelectStupidSort(number, left, right, which_one);
   }
   std::vector<int> help(5);
   std::vector<int> medians_of_small_massives((right - left + 4) / 5);
@@ -94,34 +90,37 @@ int DQS(std::vector<int>& number, int left, int right, int which_one) {
   }
   if (!is_divisible_by_five) {
     medians_of_small_massives[(right - left + 4) / 5 - 1] =
-        SmallQuickSelect(help, 0, (int)help.size(), help.size() / 2 + 1);
+        SmallQuickSelectStupidSort(help, 0, static_cast<int>(help.size()),
+                                   help.size() / 2 + 1);
   }
-  int pivot = DQS(medians_of_small_massives, 0, (right - left + 4) / 5,
-                  (right - left + 4) / kTen + 1);
+  int pivot =
+      DQuickSelect(medians_of_small_massives, 0, (right - left + 4) / 5,
+                   (right - left + 4) / kSmallerMassiveMedianHelpConst + 1);
   std::pair<int, int> pointer = Partition(number, left, right, pivot);
   if (pointer.first >= which_one - 1) {
-    return DQS(number, left, pointer.first + left + 1, which_one);
+    return DQuickSelect(number, left, pointer.first + left + 1, which_one);
   }
   if (pointer.second < which_one - 1) {
-    return DQS(number, pointer.second + left + 1, right,
-               which_one - pointer.second - 1);
+    return DQuickSelect(number, pointer.second + left + 1, right,
+                        which_one - pointer.second - 1);
   }
   return pivot;
 }
 
 int main() {
-  const int kOneTwoThree = 123;
-  const int kFourFive = 45;
-  const int kTenMillionFourThreeTwoOne = 10004321;
+  const int kFirstRecursiveConst = 123;
+  const int kSecondRecursiveConst = 45;
+  const int kThirdRecursiveConst = 10004321;
   int size;
   int which_one;
   std::cin >> size >> which_one;
   std::vector<int> number(size);
   std::cin >> number[0] >> number[1];
   for (int i = 2; i < size; ++i) {
-    number[i] = (number[i - 1] * kOneTwoThree + number[i - 2] * kFourFive) %
-                kTenMillionFourThreeTwoOne;
+    number[i] = (number[i - 1] * kFirstRecursiveConst +
+                 number[i - 2] * kSecondRecursiveConst) %
+                kThirdRecursiveConst;
   }
-  std::cout << DQS(number, 0, size, which_one) << '\n';
+  std::cout << DQuickSelect(number, 0, size, which_one) << '\n';
   return 0;
 }
