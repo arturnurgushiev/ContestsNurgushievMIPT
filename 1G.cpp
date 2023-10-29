aadcbac02c43aed7d4ff15d512b19d5e65191c6d
arturnurgushiev <142039719+arturnurgushiev@users.noreply.github.com>
2023-10-29 11:04:34 +0300

    Update 1G.cpp

--- 1G.cpp
+++ 1G.cpp
@@ -1,34 +1,36 @@
-#include <cstdio>
 #include <iostream>
 #include <vector>
 
-const int kBase = 256;
-
-void CountingSort(std::vector<std::pair<long long, long long>>& number,
-                  std::vector<std::pair<long long, long long>>& copy_number,
-                  int& size) {
-  std::vector<int> cnt(kBase);
+void LSDSort(std::vector<long long>& initialnumber, int size) {
+  std::vector<std::pair<long long, long long>> number(size);
+  std::vector<std::pair<long long, long long>> copy_number(size);
+  long long max_num = -1;
   for (int i = 0; i < size; ++i) {
-    ++cnt[number[i].first % kBase];
+    number[i] = {initialnumber[i], initialnumber[i]};
+    copy_number[i] = {initialnumber[i], initialnumber[i]};
+    max_num = std::max(max_num, number[i].second);
   }
-  for (int i = 1; i < kBase; ++i) {
-    cnt[i] += cnt[i - 1];
-  }
-  for (int i = size - 1; i >= 0; --i) {
-    copy_number[--cnt[number[i].first % kBase]] = {number[i].first / kBase,
-                                                   number[i].second};
-  }
-  for (int i = size - 1; i >= 0; --i) {
-    number[i] = copy_number[i];
-  }
-}
-
-void LSDSort(std::vector<std::pair<long long, long long>>& number,
-             std::vector<std::pair<long long, long long>>& copy_number,
-             int& size, long long& max_num) {
+  const int kBase = 256;
+  std::vector<int> cnt(kBase);
   while (max_num > 0) {
-    CountingSort(number, copy_number, size);
+    for (int i = 0; i < size; ++i) {
+      ++cnt[number[i].first % kBase];
+    }
+    for (int i = 1; i < kBase; ++i) {
+      cnt[i] += cnt[i - 1];
+    }
+    for (int i = size - 1; i >= 0; --i) {
+      copy_number[--cnt[number[i].first % kBase]] = {number[i].first / kBase,
+                                                     number[i].second};
+    }
+    number = copy_number;
     max_num /= kBase;
+    for (int i = 0; i < kBase; ++i) {
+      cnt[i] = 0;
+    }
+  }
+  for (int i = 0; i < size; ++i) {
+    initialnumber[i] = number[i].second;
   }
 }
 
@@ -37,20 +39,13 @@ int main() {
   std::cin.tie(0);
   int size;
   std::cin >> size;
-  std::vector<std::pair<long long, long long>> number(size);
-  long long max_num = -1;
+  std::vector<long long> number(size);
   for (int i = 0; i < size; ++i) {
-    std::cin >> number[i].first;
-    number[i].second = number[i].first;
-    max_num = std::max(max_num, number[i].second);
+    std::cin >> number[i];
   }
-  std::vector<std::pair<long long, long long>> copy_numbers(size);
+  LSDSort(number, size);
   for (int i = 0; i < size; ++i) {
-    copy_numbers[i] = number[i];
-  }
-  LSDSort(number, copy_numbers, size, max_num);
-  for (int i = 0; i < size; ++i) {
-    std::cout << number[i].second << '\n';
+    std::cout << number[i] << '\n';
   }
   return 0;
 }
