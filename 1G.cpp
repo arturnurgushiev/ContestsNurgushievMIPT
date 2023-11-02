e9dd392001e18b5c3f2484463527ba68754d9fe9
arturnurgushiev <142039719+arturnurgushiev@users.noreply.github.com>
2023-11-02 15:18:25 +0300

    Update 1G.cpp

--- 1G.cpp
+++ 1G.cpp
@@ -1,42 +1,42 @@
 #include <iostream>
 #include <vector>
 
-void LSDSort(std::vector<long long>& initialnumber, int size) {
+void LSDSort(std::vector<long long>& initial_number, int size) {
   std::vector<std::pair<long long, long long>> number(size);
   std::vector<std::pair<long long, long long>> copy_number(size);
-  long long max_num = -1;
+  long long max_number = -1;
   for (int i = 0; i < size; ++i) {
-    number[i] = {initialnumber[i], initialnumber[i]};
-    copy_number[i] = {initialnumber[i], initialnumber[i]};
-    max_num = std::max(max_num, number[i].second);
+    number[i] = {initial_number[i], initial_number[i]};
+    copy_number[i] = {initial_number[i], initial_number[i]};
+    max_number = std::max(max_number, number[i].second);
   }
   const int kBase = 256;
-  std::vector<int> cnt(kBase);
-  while (max_num > 0) {
+  std::vector<int> count(kBase);
+  while (max_number > 0) {
     for (int i = 0; i < size; ++i) {
-      ++cnt[number[i].first % kBase];
+      ++count[number[i].first % kBase];
     }
     for (int i = 1; i < kBase; ++i) {
-      cnt[i] += cnt[i - 1];
+      count[i] += count[i - 1];
     }
     for (int i = size - 1; i >= 0; --i) {
-      copy_number[--cnt[number[i].first % kBase]] = {number[i].first / kBase,
+      copy_number[--count[number[i].first % kBase]] = {number[i].first / kBase,
                                                      number[i].second};
     }
     number = copy_number;
-    max_num /= kBase;
+    max_number /= kBase;
     for (int i = 0; i < kBase; ++i) {
-      cnt[i] = 0;
+      count[i] = 0;
     }
   }
   for (int i = 0; i < size; ++i) {
-    initialnumber[i] = number[i].second;
+    initial_number[i] = number[i].second;
   }
 }
 
 int main() {
   std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
+  std::cin.tie(nullptr);
   int size;
   std::cin >> size;
   std::vector<long long> number(size);
